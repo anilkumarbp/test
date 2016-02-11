3a7984fdbc639e8043d3d920eb32f217e004f10d
Anil Kumar <anil.akbp@gmail.com>
2016-02-10 17:06:36 -0800

    no test scheme

---
+++ testing/testing/testing.h
@@ -0,0 +1,19 @@
+//
+//  testing.h
+//  testing
+//
+//  Created by Anil Kumar BP on 2/10/16.
+//  Copyright © 2016 Anil Kumar BP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testing.
+FOUNDATION_EXPORT double testingVersionNumber;
+
+//! Project version string for testing.
+FOUNDATION_EXPORT const unsigned char testingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testing/PublicHeader.h>
+
+
